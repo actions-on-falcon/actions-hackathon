a7e3a0758818c81284d24f24cff6ddadfaf77b5f
Phumrapee Limpianchop <rayriffy@gmail.com>
2018-12-15 14:52:13 +0700

    Add features to visiting intent (#5)

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,20 @@
-import chalk from 'chalk'
+import {dialogflow, SimpleResponse, BasicCard} from 'actions-on-google'
 import basicAuth from 'basic-auth-connect'
-import {dialogflow, SimpleResponse} from 'actions-on-google'
+import chalk from 'chalk'
+import axios from 'axios'
 
 const app = dialogflow({debug: false})
+
+const api = axios.create({
+  baseURL: ''
+})
+
+function isSpeaker(conv) {
+  if(conv.surface.capabilities.has('actions.capability.SCREEN_OUTPUT'))
+    return false
+  else
+    return true
+}
 
 app.intent('welcome', conv => {
   console.log(chalk.green('info:'), 'Hit welcome intent')
@@ -16,12 +28,37 @@ app.intent('welcome', conv => {
 })
 
 app.intent('visiting', conv => {
-  console.log(chalk.green('info:'), 'Hit welcome intent')
+  console.log(chalk.green('info:'), 'Hit visiting intent')
 
-  const response = new SimpleResponse({
-    speech: 'Hello',
-    text: 'Hello',
-  })
+  var qrcode = 'ABCDEFG'
+
+  // api.post('/api', {
+  //   name: conv.parameters.name,
+  //   time: conv.parameters.time
+  // })
+  // .then(
+  //   console.log(response)
+  // )
+
+  var response
+
+  if(isSpeaker(conv) === false) {
+    response = new BasicCard({
+      title: 'Visitor access code created!',
+      subtitle: 'Code is ' + qrcode,
+      image: new Image({
+        url: 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + qrcode,
+        alt: qrcode,
+      }),
+      buttons: new Button({
+        title: 'Open on web browser',
+        url: 'https://assistant.google.com/',
+      }),
+    })
+  }
+  else {
+    response = '<speak>Visitor code is created<break time="400ms"/>Your <say-as interpret-as="characters">ID</say-as> is <say-as interpret-as="characters">' + qrcode + '</say-as></speak>'
+  }
 
   conv.ask(response)
 })
