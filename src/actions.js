c7b8146eda342ee428b099d558559f5496475439
Phumrapee Limpianchop <git@rayriffy.com>
2018-12-15 12:59:29 +0700

    :sparkles: Add intents

--- src/actions.js
+++ src/actions.js
@@ -2,4 +2,20 @@ import {dialogflow} from 'actions-on-google'
 
 const app = dialogflow({debug: false})
 
+app.intent('welcome', (conv) => {
+  console.log('\x1b[33minfo:\x1b[0m Hit welcome intent');
+  conv.ask(new SimpleResponse({
+    speech: 'Hello',
+    text: 'Hello',
+  }));
+});
+
+app.intent('visiting', (conv) => {
+  console.log('\x1b[33minfo:\x1b[0m Hit welcome intent');
+  conv.ask(new SimpleResponse({
+    speech: 'Hello',
+    text: 'Hello',
+  }));
+});
+
 export default app
