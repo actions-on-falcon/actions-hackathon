396bf290bfb0bf7a0f0d39cbd4936b1c4ea27b28
Pmc Dev <phoomparin@gmail.com>
2018-12-15 11:46:20 +0700

    :recycle: Minor Refactor

--- src/index.js
+++ src/index.js
@@ -2,17 +2,22 @@ import {dialogflow} from 'actions-on-google'
 import bodyParser from 'body-parser'
 import express from 'express'
 import basicAuth from 'basic-auth-connect'
-import rp from 'request-promise'
-import i18n from 'i18n'
 import dotenv from 'dotenv'
+
+import selfPing from 'heroku-self-ping'
 
 dotenv.config()
 
-require('heroku-self-ping')("https://actions-on-falcon.herokuapp.com/")
+selfPing('https://actions-on-falcon.herokuapp.com/')
 
-const app = dialogflow({debug: false});
+const server = express()
+const app = dialogflow({debug: false})
 
-express()
-  .use(basicAuth(process.env.HTTP_USER, process.env.HTTP_PASS))
-  .use(bodyParser.json(), app)
-  .listen(process.env.PORT);
+const {HTTP_USER, HTTP_PASS} = process.env
+
+server.use(basicAuth(HTTP_USER, HTTP_PASS))
+
+server.use(bodyParser.json())
+server.use(app)
+
+server.listen(process.env.PORT)
